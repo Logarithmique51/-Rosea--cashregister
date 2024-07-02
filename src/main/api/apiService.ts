c3d7ce0038c749b56dc89012bdbf4bc28fe76788
Logarithmique51 <74143691+Logarithmique51@users.noreply.github.com>
2024-07-02 23:21:51 +0200

    menu created

--- src/main/api/apiService.ts
+++ src/main/api/apiService.ts
@@ -1,12 +1,22 @@
-import axios from "axios";
+import axios from 'axios';
 
-const API_URL = 'localhost:3000'
+const API_URL = 'localhost:3000';
 
+// eslint-disable-next-line import/prefer-default-export
 export const getPizza = async () => {
   try {
-    const response = await axios.get(`${API_URL}/item`)
+    const response = await axios.get(`${API_URL}/item`);
     return response.data;
   } catch (error) {
     console.error('Error fetching');
   }
-}
+};
+
+export const getMenu = async () => {
+  try {
+    const response = await axios.get(`${API_URL}/menu`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching');
+  }
+};
--- src/main/main.ts
+++ src/main/main.ts
@@ -41,12 +41,49 @@ interface Pizza {
   categorie: string;
 }
 
+interface Menu {
+  id:number;
+  title:string;
+  price: number;
+  slug : string;
+}
+
+function shuffle(array) {
+  let currentIndex = array.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex !== 0) {
+    // Pick a remaining element...
+    const randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+}
+
 ipcMain.on('getPizza', async (event) => {
   try {
     const response = await axios.get<Pizza[]>('http://localhost:3000/item');
-     event.reply('getPizza', response.data);
+    const itemarray = response.data;
+    shuffle(itemarray);
+    event.reply('getPizza', response.data);
   } catch (error) {
     event.reply('getPizza', []);
+  }
+});
+
+ipcMain.on('getMenu', async (event) => {
+  try {
+    const response = await axios.get<Menu[]>('http://localhost:3000/menu');
+    const itemarray = response.data;
+    shuffle(itemarray);
+    event.reply('getMenu', response.data);
+  } catch (error) {
+    event.reply('getMenu', []);
   }
 });
 
